24fc3c1ff8e6a7f2c9f6471549ac236ee5cafc38
YID <yi19871230@hotmail.com>
2021-07-07 22:53:56 +0800

    Refactor code into plpeline design pattern

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,38 +1,22 @@
-import urllib.request
-import json
-from yt_concate.api import API_KEY
+from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.pipeline.stops.get_video_list import GetVideoList
+from yt_concate.pipeline.stops.step import StepException
+
+
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID,
+    }
+    steps = [
+        GetVideoList(),
+        ]
 
-def get_all_video_in_channel(channel_id):
-    api_key = API_KEY
-
-
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
-                                                                                                        channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url, timeout=1)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-video_list = get_all_video_in_channel(CHANNEL_ID)
-
-print(len(video_list))
+if __name__ == '__main__':
+    main()
